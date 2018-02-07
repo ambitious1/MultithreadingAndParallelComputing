b49986becbb57f8685cf659c550963e8199cc19e
Dacosta, Travis <Travis.Dacosta1@T-Mobile.com>
2018-02-07 10:29:17 -0800

    Push both projects, Lesson 1-SequentialProcessing and Lesson 2-Runnable Threads

---
+++ SequentialProcessing/src/sequentialprocessing/SequentialProcessing.java
@@ -0,0 +1,35 @@
+package sequentialprocessing;
+
+/**
+ * @author Travis
+ **/
+
+class Runner1{
+      void startRunning(){
+          for(int i = 0; i<10; i++){
+              System.out.println("Runner1: "+i);
+          }
+      }
+  }
+  
+  class Runner2{
+      void startRunning(){
+          for(int i = 0; i<10; i++){
+              System.out.println("Runner2: "+i);
+          }
+      }
+  }
+  
+
+public class SequentialProcessing {
+  
+    public static void main(String[] args) {
+        Runner1 runner1 = new Runner1();
+        Runner2 runner2 = new Runner2();
+        
+        runner1.startRunning();
+        runner2.startRunning();
+        
+    }
+    
+}
